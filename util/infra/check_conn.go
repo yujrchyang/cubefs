f5c33b9d00ebb1c33840244d5c9102c5441928e2
bjguoweilong <guoweilong@jd.com>
2024-07-25 16:54:21 +0800

    optimize: checkConn
    Signed-off-by: bjguoweilong <guoweilong@jd.com>

--- util/infra/check_conn.go
+++ util/infra/check_conn.go
@@ -1,11 +1,15 @@
 package infra
 
 import (
+	"fmt"
 	"net"
 	"time"
 )
 
-func CheckConn(conn net.Conn) (err error) {
+func CheckConnIsAvali(conn net.Conn) (err error) {
+	if conn == nil {
+		return fmt.Errorf("conn is nil")
+	}
 	err = conn.SetWriteDeadline(time.Now().Add(time.Millisecond * 50))
 	if err != nil {
 		return
