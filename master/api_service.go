cbef6c454705ddd3c00bdb5356a931f48f68830b
lizhenzhen36 <lizhenzhen36@jd.com>
2024-08-28 13:34:32 +0800

    check bitmap frozen hours value

--- master/api_service.go
+++ master/api_service.go
@@ -4283,6 +4283,10 @@ func parseBitMapSnapFrozenHourToUpdateVol(r *http.Request, vol *Vol) (bitMapSnap
 	if err != nil {
 		return
 	}
+	if interval < 0 {
+		err = fmt.Errorf("%s invalid value: %v", proto.BitMapSnapFrozenHour, interval)
+		return
+	}
 	bitMapSnapFrozenHour = interval
 	return
 }
