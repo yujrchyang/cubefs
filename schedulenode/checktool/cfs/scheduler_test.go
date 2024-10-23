d073a6c17ed7596713c44ac8ad197ee2ab041f88
xuxihao1 <xuxihao3@jd.com>
2024-10-23 21:15:27 +0800

    fix: change check unavailable data partition scheduler interval from 1 min to 15 min
    Signed-off-by: xuxihao1 <xuxihao3@jd.com>

--- schedulenode/checktool/cfs/scheduler_test.go
+++ schedulenode/checktool/cfs/scheduler_test.go
@@ -14,3 +14,11 @@ func TestScheduler(t *testing.T) {
 		fmt.Printf("echo schedule\n")
 	}, time.Second*1)
 }
+
+func TestScheduleV2(t *testing.T) {
+	monitor := NewChubaoFSMonitor(context.Background())
+	monitor.NewScheduleV2(func() {
+		time.Sleep(time.Second * 10)
+		fmt.Printf("echo schedule\n")
+	}, time.Second*1)
+}
