185148311edc260206fd013c93641112e6c4dc77
xiaopeng42 <xiaopeng42@jd.com>
2024-10-28 18:26:39 +0800

    feat: send JMQ message in client alarm

--- schedulenode/checktool/cfs/client_alarm.go
+++ schedulenode/checktool/cfs/client_alarm.go
@@ -1,42 +1,76 @@
 package cfs
 
 import (
+	"encoding/json"
 	"fmt"
+	"regexp"
 	"strings"
 	"time"
 
+	"github.com/Shopify/sarama"
 	"github.com/cubefs/cubefs/util/checktool/mdc"
 	"github.com/cubefs/cubefs/util/checktool/ump"
+	"github.com/cubefs/cubefs/util/log"
 )
 
 const (
-	clientAlarmInterval = 60
+	clientAlarmInterval     = 60 * time.Second
+	clusterMysql            = "mysql"
+	clusterSpark            = "spark"
+
+	cfgClientJMQAddr  = "clientJMQAddr"
+	cfgClientJMQTopic = "clientJMQTopic"
+	cfgClientJMQGroup = "clientJMQGroup"
 )
 
-func (s *ChubaoFSMonitor) clientAlarm() {
-	now := time.Now().Unix()
-	begin := now / clientAlarmInterval * clientAlarmInterval
-	end := begin + int64(clientAlarmInterval)
-	s.clientAlarmImpl(umpKeyMysqlClientPrefix, begin, end)
-	s.clientAlarmImpl(umpKeySparkClientPrefix, begin, end)
+type clientJMQConfig struct {
+	addr  string
+	topic string
+	group string
 }
 
-func (s *ChubaoFSMonitor) clientAlarmImpl(prefix string, begin int64, end int64) (alarmCount int) {
+type clientJMQMsg struct {
+	Cluster string `json:"cluster"`
+	Volume  string `json:"volume"`
+	Ip      string `json:"ip"`
+	Error   string `json:"error"`
+}
+
+func (s *ChubaoFSMonitor) clientAlarm() {
+	// only execute on one node
+	if s.clientJMQConfig.addr == "" {
+		return
+	}
+	now := time.Now()
+	begin := now.Add(-clientAlarmInterval - 5*time.Second)
+	end := now
+	s.clientAlarmImpl(clusterMysql, begin, end)
+	s.clientAlarmImpl(clusterSpark, begin, end)
+}
+
+func (s *ChubaoFSMonitor) clientAlarmImpl(cluster string, begin time.Time, end time.Time) (alarmCount int) {
 	var (
+		prefix       string
 		alarmRecords *ump.AlarmRecordResponse
 		err          error
 	)
+	if cluster == clusterMysql {
+		prefix = umpKeyMysqlClientPrefix
+	} else if cluster == clusterSpark {
+		prefix = umpKeySparkClientPrefix
+	}
 	for i := 0; i < 10; i++ {
-		alarmRecords, err = s.umpClient.GetAlarmRecords(alarmRecordsMethod, UmpAppName, UmpPlatForm, prefix+umpKeyWarningSufix, begin*1000, end*1000)
+		alarmRecords, err = s.umpClient.GetAlarmRecords(alarmRecordsMethod, UmpAppName, UmpPlatForm, prefix+umpKeyWarningSufix, begin.UnixMilli(), end.UnixMilli())
 		if err == nil {
 			break
 		}
 		time.Sleep(time.Second)
 	}
 	if err != nil || alarmRecords == nil {
+		log.LogErrorf("clientAlarm: GetAlarmRecords failed")
 		return
 	}
-	ignoreContents := []string{"no such file or directory", "/readProcess/register", "timeout", "NotExistErr", "network is unreachable"}
+	ignoreContents := []string{"no such file or directory", "/readProcess/register", "timeout", "NotExistErr", "network is unreachable", "not exists"}
 	for _, record := range alarmRecords.Records {
 		var ignore bool
 		for _, ignoreContent := range ignoreContents {
@@ -48,18 +82,49 @@ func (s *ChubaoFSMonitor) clientAlarmImpl(prefix string, begin int64, end int64)
 		if ignore {
 			continue
 		}
+		vol, ip := parseClientWarning(record.Content)
+		abnormal := isIpPingAbnormal(ip, record.AlarmTime-300*1000, record.AlarmTime+60*1000)
+		if abnormal {
+			continue
+		}
 		ipRes := strings.Split(record.Content, "报警主机")
 		if len(ipRes) > 1 {
-			ip := strings.Trim(ipRes[1], ":： ")
-			abnormal := isIpPingAbnormal(ip, record.AlarmTime-300*1000, record.AlarmTime+60*1000)
-			if abnormal {
-				continue
-			}
 			// put host first to avoid being truncated
 			record.Content = fmt.Sprintf("报警主机：%s，%s", ip, strings.Trim(ipRes[0], ",，"))
 		}
 		alarmCount++
 		warnBySpecialUmpKeyWithPrefix(prefix+umpKeyFatalSufix, record.Content)
+		log.LogInfof("clientAlarm: ump(%v) content(%v)", prefix+umpKeyFatalSufix, record.Content)
+		insertRes := s.insertClientMysqlMsg(cluster, vol, ip, record.Content, end.Add(-time.Hour), end)
+		if insertRes {
+			msg, err := json.Marshal(clientJMQMsg{Cluster: cluster, Volume: vol, Ip: ip, Error: record.Content})
+			if err == nil {
+				sendClientMQMsg(s.clientJMQConfig.addr, s.clientJMQConfig.topic, s.clientJMQConfig.group, string(msg))
+			} else {
+				log.LogErrorf("clientAlarm: marshal failed, err(%v)", err)
+			}
+		}
+	}
+	return
+}
+
+func parseClientWarning(content string) (vol string, ip string) {
+	volRegex := regexp.MustCompile(`volume\([^()]*\)`)
+	re := volRegex.FindStringSubmatch(content)
+	if len(re) > 0 {
+		volSlice := strings.FieldsFunc(re[0], func(r rune) bool { return r == '(' || r == ')' })
+		if len(volSlice) > 1 {
+			vol = volSlice[1]
+		}
+	}
+
+	ipRegex := regexp.MustCompile(`报警主机[:：][0-9.]*`)
+	re = ipRegex.FindStringSubmatch(content)
+	if len(re) > 0 {
+		ipSlice := strings.FieldsFunc(re[0], func(r rune) bool { return r == ':' || r == '：' })
+		if len(ipSlice) > 1 {
+			ip = ipSlice[1]
+		}
 	}
 	return
 }
@@ -100,4 +165,55 @@ func isIpPingAbnormal(ip string, begin int64, end int64) bool {
 		}
 	}
 	return false
+}
+
+func (s *ChubaoFSMonitor) insertClientMysqlMsg(cluster string, volume string, ip string, content string, begin time.Time, end time.Time) bool {
+	var count int64
+	msg := fmt.Sprintf("cluster(%v) volume(%v) ip(%v) content(%v) begin(%v) end(%v)", cluster, volume, ip, content, begin, end)
+	s.sreDB.Table("client_fatal").Where("cluster = ? and volume = ? and ip = ? and create_time between ? and ?", cluster, volume, ip, begin, end).Count(&count)
+	if count > 0 {
+		log.LogInfof("clientAlarm: no need to insert mysql, %v", msg)
+		return false
+	}
+	result := s.sreDB.Exec("insert into client_fatal (cluster,volume,ip,content) values (?,?,?,?)", cluster, volume, ip, content)
+	if result.Error != nil {
+		log.LogErrorf("clientAlarm: insert mysql failed, %v err(%v)", msg, result.Error)
+		return false
+	} else {
+		log.LogInfof("clientAlarm: insert mysql, %v", msg)
+	}
+	return true
+}
+
+func sendClientMQMsg(address string, topic string, clientId string, msgVal string) {
+	msg := fmt.Sprintf("address(%v) topic(%v) clientId(%v) msgVal(%v)", address, topic, clientId, msgVal)
+	if address == "" || topic == "" || clientId == "" {
+		log.LogErrorf("clientAlarm: no jmq config, %v", msg)
+		return
+	}
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	config.ClientID = clientId
+	producer, err := sarama.NewSyncProducer([]string{address}, config)
+	defer func() {
+		if producer != nil {
+			producer.Close()
+		}
+	}()
+	if err != nil {
+		log.LogErrorf("clientAlarm: newMQProducer failed, err(%v)", err)
+		return
+	}
+
+	partition, offset, err := producer.SendMessage(&sarama.ProducerMessage{Topic: topic, Value: sarama.StringEncoder(msgVal)})
+	if err != nil {
+		log.LogErrorf("clientAlarm: SendMessage failed, %v err(%v)", msg, err)
+		return
+	} else {
+		log.LogInfof("clientAlarm: SendMessage, %v partition(%v) offset(%v)", msg, partition, offset)
+	}
+	return
 }
--- schedulenode/checktool/cfs/client_alarm_test.go
+++ schedulenode/checktool/cfs/client_alarm_test.go
@@ -7,20 +7,30 @@ import (
 	"github.com/cubefs/cubefs/util/checktool/ump"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func TestClientAlarm(t *testing.T) {
 	m := NewChubaoFSMonitor(context.Background())
 	m.umpClient = ump.NewUmpClient("", umpOpenAPiDomain)
-	begin := time.Date(2024, 10, 14, 21, 12, 0, 0, time.Local).Unix()
-	alarmCount := m.clientAlarmImpl(umpKeySparkClientPrefix, begin, begin+600)
+	m.clientJMQConfig = clientJMQConfig{addr: "", topic: "", group: ""}
+	m.sreDB, _ = gorm.Open(mysql.New(mysql.Config{DSN: ""}), &gorm.Config{})
+	begin := time.Date(2024, 10, 14, 21, 12, 0, 0, time.Local)
+	alarmCount := m.clientAlarmImpl(clusterSpark, begin, begin.Add(10*time.Minute))
 	assert.NotZero(t, alarmCount)
-	begin = time.Date(2024, 10, 15, 13, 17, 0, 0, time.Local).Unix()
-	alarmCount = m.clientAlarmImpl(umpKeySparkClientPrefix, begin, begin+600)
+	begin = time.Date(2024, 10, 15, 13, 17, 0, 0, time.Local)
+	alarmCount = m.clientAlarmImpl(clusterSpark, begin, begin.Add(10*time.Minute))
 	assert.Zero(t, alarmCount)
 
 	// network of ip is not available
-	begin = time.Date(2024, 10, 21, 07, 51, 0, 0, time.Local).Unix()
-	alarmCount = m.clientAlarmImpl(umpKeySparkClientPrefix, begin, begin+600)
+	begin = time.Date(2024, 10, 21, 07, 51, 0, 0, time.Local)
+	alarmCount = m.clientAlarmImpl(clusterSpark, begin, begin.Add(10*time.Minute))
 	assert.Zero(t, alarmCount)
+}
+
+func TestParseClientWarning(t *testing.T) {
+	content := "volume(rd-finance-m-prd-ht87a-xx-20) act(cfs_pwrite_inode) - id(1) ino(33554770) size(131072) offset(2097152) re(-5) err(no such file or directory)，报警主机：11.145.226.59"
+	vol, ip := parseClientWarning(content)
+	assert.True(t, vol == "rd-finance-m-prd-ht87a-xx-20" && ip == "11.145.226.59")
 }
