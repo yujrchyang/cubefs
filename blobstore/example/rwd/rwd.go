828097b8231f52af40cb9e9f9b023bcf6a994367
yujrchyang <yujrchyang@gmail.com>
2025-11-05 09:03:03 +0800

    feat: add read/write/delete basic operation

---
+++ blobstore/example/rwd/rwd.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"code.cloudfoundry.org/bytefmt"
+	"github.com/cubefs/cubefs/blobstore/api/access"
+	"github.com/cubefs/cubefs/blobstore/sdk"
+)
+
+const (
+	objectNamePrefix = "blobstore_rwd_"
+)
+
+var (
+	option      string
+	dbDir       string
+	config      string
+	objectSize  string
+	objectBytes uint64
+	objectNum   int
+	objectData  []byte
+)
+
+func checkParameters() {
+	allowedOpt := map[string]struct{}{
+		"wrd": {},
+	}
+	if _, ok := allowedOpt[option]; !ok {
+		log.Fatalf("invalid option: %s", option)
+	}
+	if config == "" {
+		log.Fatalf("please input sdk config file")
+	}
+	info, err := os.Stat(config)
+	if err != nil {
+		log.Fatalf("failed to get config file(%s) stat info: %v", config, err)
+	}
+	if info.IsDir() {
+		log.Fatalf("config file(%s) is a directory", config)
+	}
+	if s, err := bytefmt.ToBytes(objectSize); err != nil {
+		log.Fatalf("failed to convert objectSize(%s): %v", objectSize, err)
+	} else {
+		objectBytes = s
+	}
+	if objectBytes <= 0 {
+		log.Fatalf("invalid objectSize: %d", objectSize)
+	}
+	if objectNum <= 0 {
+		log.Fatalf("invalid objectNum: %d", objectNum)
+	}
+	if err := os.MkdirAll(dbDir, 0o755); err != nil {
+		log.Fatalf("failed to create db dir(%s): %v", dbDir, err)
+	}
+}
+
+func prepareObjectData() {
+	objectData = make([]byte, objectBytes)
+	rand.Read(objectData)
+	hasher := sha256.New()
+	hasher.Write(objectData)
+	_ = base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+}
+
+func init() {
+	flag.StringVar(&option, "o", "wrd", "The action you want to perform")
+	flag.StringVar(&dbDir, "d", "/tmp/blobstore_rwd_test", "Rocksdb database directory")
+	flag.StringVar(&config, "c", "", "SDK configuration file path")
+	flag.StringVar(&objectSize, "s", "1M", "The size of the object with postfix K, M, and G")
+	flag.IntVar(&objectNum, "n", 1, "The number of objects you want to write")
+	flag.Parse()
+
+	checkParameters()
+	prepareObjectData()
+}
+
+type RWDStore struct {
+	ctx    context.Context
+	client access.API
+}
+
+func (s *RWDStore) putObject(data io.Reader, size int64) (loc access.Location, err error) {
+	args := &access.PutArgs{
+		Size:   size,
+		Hashes: access.HashAlgDummy,
+		Body:   data,
+	}
+
+	loc, _, err = s.client.Put(s.ctx, args)
+	if err != nil {
+		return access.Location{}, err
+	}
+
+	return loc, nil
+}
+
+func (s *RWDStore) getObject(loc access.Location, size int64) error {
+	args := &access.GetArgs{
+		Location: loc,
+		Offset:   0,
+		ReadSize: uint64(size),
+	}
+
+	rc, err := s.client.Get(s.ctx, args)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	// Write data to the provided io.Writer
+	writer := io.Discard
+	_, err = io.Copy(writer, rc)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *RWDStore) delObject(loc access.Location) error {
+	args := &access.DeleteArgs{
+		Locations: []access.Location{loc},
+	}
+	_, err := s.client.Delete(s.ctx, args)
+	return err
+}
+
+func (s *RWDStore) doWriteReadDelete() {
+	obj := bytes.NewReader(objectData)
+
+	for i := 1; i <= objectNum; i++ {
+		objName := fmt.Sprintf("%s%d", objectNamePrefix, i)
+		loc, err := s.putObject(obj, obj.Size())
+		if err != nil {
+			log.Fatalf("failed to put object(%s) to blobstore: %v", objName, err)
+		}
+
+		err = s.getObject(loc, obj.Size())
+		if err != nil {
+			log.Fatalf("failed to get object(%s) from blobstore: %v", objName, err)
+		}
+
+		err = s.delObject(loc)
+		if err != nil {
+			log.Fatalf("failed to del object(%s) from blobstore: %v", objName, err)
+		}
+	}
+}
+
+func initRWDStore() *RWDStore {
+	// init context
+	ctx := context.Background()
+
+	// init BlobStore client
+	configData, err := os.ReadFile(config)
+	if err != nil {
+		log.Fatalf("failed to read config file(%s): %v", config, err)
+	}
+	var conf sdk.Config
+	if err := json.Unmarshal(configData, &conf); err != nil {
+		log.Fatalf("failed to parse config file(%s): %v", config, err)
+	}
+	client, err := sdk.New(&conf)
+	if err != nil {
+		log.Fatalf("failed to create blobstore client: %v", err)
+	}
+
+	return &RWDStore{
+		ctx:    ctx,
+		client: client,
+	}
+}
+
+func main() {
+	rwdClient := initRWDStore()
+
+	switch option {
+	case "wrd":
+		rwdClient.doWriteReadDelete()
+	default:
+		log.Fatalf("invalid option: %s", option)
+	}
+}
